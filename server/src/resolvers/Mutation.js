9e132f70102eb3e8f2eee8f35775145043b306dd
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-18 14:13:16 +0530

    Added Mutation

---
+++ server/src/resolvers/Mutation.js
@@ -0,0 +1,39 @@
+async function signup(parents, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10);
+
+  const user = await context.prisma.user.create({
+    data: { ...args, password },
+  });
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+}
+
+async function login(parents, args, context, info) {
+  const user = await context.prisma.user.findUnique({
+    where: {
+      email: args.email,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found!");
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+
+  return {
+    token,
+    user,
+  };
+}
+
+module.exports = {
+  signup,
+  login,
+  post,
+};
---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,5 @@
+function feed(parents, args, context, info) {
+  return context.prisma.link.findMany();
+}
+
+module.exports = { feed };
